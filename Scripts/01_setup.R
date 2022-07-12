ccb72c5fec9eaedb557fe64ad66e86ee79dffc1e
steffilazerte <steffi@steffi.ca>
2022-07-12 15:08:08 -0500

    Add installation instructions where it matters.

--- Scripts/01_setup.R
+++ Scripts/01_setup.R
@@ -8,4 +8,13 @@ dir.create("Results")
 dir.create("Results/Figures")
 
 
-# Setup done
+# Install specific package versions:
+
+# dggridR was archived on CRAN, can either use specific version, or GitHub version
+#devtools::install_version("dggridR", version = "2.0.4", repos = "http://cran.us.r-project.org")
+
+# SDMTools was archived on CRAN
+#devtools::install_version("SDMTools", version = "1.1-221", repos = "http://cran.us.r-project.org")
+
+# We require a specific version of segmented to reproduce results exactly
+#devtools::install_version("segmented", version = "1.2-0", repos = "http://cran.us.r-project.org")
